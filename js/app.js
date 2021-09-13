e63a19a86469936b1e55016b67633197a7f63bb7
Nurunnahar Nody <nurunnaharnody@gmail.com>
2021-09-13 15:09:05 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -16,11 +16,17 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${product.image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h3 class="product-title">${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <p class="icon-detail"> <span class="icon-color"><i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star-half-alt"></i></span> ${product.rating.rate}</p>
+      <p class="icon-detail"><i class="fas fa-user"></i> ${product.rating.count}</P>
+      <h2 class="product-price">Price: $ ${product.price}</h2>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn">add to cart</button>
+      <button id="details-btn" class="btn btn-detail">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
